48bb5a3ba4c4eb3206a488462dea3d23c1c18bb6
Olatunbosun Ifeoluwa <olatunbosunifeoluwa@gmail.com>
2023-08-16 16:09:40 +0100

    3rd deploy

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -28,21 +28,19 @@ const postStudentSignUp =  (req,res)=>{
 const postStudentSignIn =  (req,res)=>{
     studentModel.find(
         {email:req.body.email,password:req.body.password},
-        {upsert:true, new:true}
+        // {upsert:true, new:true}
         )
-
     .then((response)=>{
         console.log(response)
         if(response.length >0){
             let studentToken = jwt.sign(
                 {email: req.body.email},
                 process.env.JWTSECRET,
-                
-                
+                {expiresIn:"6h"},   
             )
-            res.send({studentToken, message: "correct"})
+            res.send({studentToken, message: "User Signed in Successfully"})
         }else{
-           
+            res.send({message:"Wrong Pass"})
         }
     })
     .catch((err)=>{
@@ -57,59 +55,85 @@ const postStudentDB = (req,res)=>{
  
 
 const getStudentInformation = (req, res) => {
-    
     let studentToken = req.headers.authorization
     // console.log(studentToken);
-    jwt.verify(studentToken, process.env.JWTSECRET, (err, result) => {
-        if (err) {
-            console.log("error");
-        } else {
-            studentModel.findOne({email: result.email})
-            .then((response) => {
-                // console.log(response);
-                res.send(response)
+    jwt.verify(studentToken,process.env.JWTSECRET,(err,result)=> {
+        if (err){
+            console.log(err)
+            res.send({message:"Error Occured", status:false})
+        }else{
+            let userEmail = result.email
+            studentModel.findOne({email: userEmail})
+            .then ((response)=>{
+                res.send({response, status:true})
             })
-        }
+        } 
     })
-    // if(studentToken== true){
-    //     // studentModel.findOne()
-    //     // .then((response) => {
-    //     //     // console.log(response);
-    //     //     res.send({status: true, response})
-    //     // })
-
-    // }
+    // let studentToken = req.headers.authorization.split(" ")[1]
+    // jwt.verify(studentToken, process.env.JWTSECRET, (err, result) => {
+    //     if (err) {
+    //         console.log("error");
+    //         res.send({message:"Error Occured", status:false})
+    //     } else {
+    //         studentModel.findOne({email: result.email})
+    //         .then((response) => {
+    //             res.send({response, status:true})
+    //         })
+    //     }
+    // })
 }
 
 
-const postUpdate =(req,res)=>{
-    let myEmail = req.body.userEmail
-    let newPassword = req.body.newPass
-    let oldPassword = req.body.password
-    // console.log(myEmail, newPass, password);
-    studentModel.findOneAndUpdate(
-        {email: myEmail}
-    )
-    .then((response)=>{
-        console.log(response);
-        if (response) {
-            if(response.password==oldPassword){  
-                response.password = newPassword
-                response.save()
-                .then((results) => {
-                    if (results) {
-                        console.log("password changed");
-                    } else {
-                        console.log("password failed to change");
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-            }
+// const postUpdate =(req,res)=>{
+//     const {oldPassword,newPassword}=req.body
+//     let myEmail = req.body.userEmail
+//     try{
+//         const user = await studentModel.findOne(myEmail);
+
+//         const passwordMatch = await bcrypt.compare(oldPassword,);
+//         if(!passwordMatch){
+//             return res.status(401).json({error: "Invalid current Password"});
+//         }
+//         const hashedPassword = await bcrypt.hash(newPassword,10);
+
+//         await. studentModel.findOneAndUpdate(myEmail, {password: hashedPassword});
+//         res.json({message: "Password changed successfully"});
+//     }
+//     catch (error){
+//         console.log(error);
+//         res.status(500).json({error: "Internal Server Error"})
+//     }
+//     });
+
+    const postUpdate = async (req, res) => {
+        const { oldPassword, newPassword } = req.body;
+        const userEmail = req.user.email; 
+        // Assuming you have implemented authentication
+      
+        try {
+          // Fetch the user from the database
+          const user = await studentModel.findOne(userEmail);
+      
+          // Validate current password
+          const passwordMatch = await bcrypt.compare(oldPassword, user.password);
+          if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid current password' });
+          }
+      
+          // Hash the new password
+          const hashedPassword = await bcrypt.hash(newPassword, 10);
+      
+          // Update the user's password in the database
+          await studentModel.findOneAndUpdate(userEmail, { password: hashedPassword });
+      
+          res.json({ message: 'Password changed successfully' });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Internal Server Error' });
         }
-    })
-}
+      };
+      
+
 
 
 
